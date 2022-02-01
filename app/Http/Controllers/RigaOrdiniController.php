651c05528ff1fda1bdb98b59065d198010a49283
Marco Tessitore <marcotexcode@gmail.com>
2022-02-01 15:37:35 +0100

    correzioni aggiunta carrello

--- app/Http/Controllers/RigaOrdiniController.php
+++ app/Http/Controllers/RigaOrdiniController.php
@@ -28,33 +28,38 @@ class RigaOrdiniController extends Controller
         ]);
         
         $idCarrello = session('idCarrello');
-
+       // dd($idCarrello);
         // Se l'utente e loggato
         if (Auth::user()) {
             // Se in sessione c'è un carrello e user_id e null (whereNull) aggiungi alla colonna user_id l'id dell utente *********** risolto
-            if(session('idCarrello') && OrdineTestata::whereNull('user_id') ) {
+            // Quando la sessione e piena e la trova con find()
+            if(session('idCarrello') && OrdineTestata::whereNull('user_id') && OrdineTestata::find(session('idCarrello'))) {
                 // aggiungi alla colonna user_id l'id dell utente
-                $CarrelloUtente = OrdineTestata::where('id', $idCarrello)->update(['user_id' => Auth::user()->id]);
+                // dd('pluto');
+                $idCarrelloUtente = OrdineTestata::where('id', $idCarrello)->update(['user_id' => Auth::user()->id]);
             
-            // Se la sessione e vuota e l'utente autenticato non a un carrello crea un carrello autenticato risolto
+            // Se la sessione e vuota o non la trova l'utente autenticato non a un carrello crea un carrello autenticato 
             } elseif (!session('idCarrello') || !OrdineTestata::find(session('idCarrello'))) {
-                $idCarrello = OrdineTestata::firstOrCreate([ 'user_id' => Auth::user()->id, 'tipo' => 0]);
-            
-            /**
-             * PRIMA DI RISOLVERE L'ULTIMO PROBLEMA CORREGGERE L'ERRORE CHE SE ELIMINO GLI ELEMENTI DAL DB, E NELLA SESSIONE RIMANE 
-             * SALVATO UN NUMERO MI DA ARRORE PERCHE NON RIESCE A TROVARLO
-             */
+                $idCarrello = OrdineTestata::firstOrCreate([ 'user_id' => Auth::user()->id, 'tipo' => 0])->id;
+                //dd('pippo');
+                
+                 /**
+                  * Quando un utente non loggato aggiunge un carrello in sessione
+                  * appena si logga quel carrello in sessione si deve trasferire nel carrello dell utente loggato
+                  */
             
             // Se l’utente loggato ha un carrello aggiungere la roba che sta nel  carrello in sessione e poi eliminarlo risolvere questo problema
             // } elseif(OrdineTestata::where('user_id' , Auth::user()->id)->get()) {
             //     dd('ciao');
             }
-
+            /**
+             *  Se non sei autenticato la sessione e vuota o non trova l'id della sessione 
+             * aggiungi un nuovo id alla sessione
+            */ 
         } elseif ($request->session()->missing('idCarrello') || !OrdineTestata::find(session('idCarrello'))) {
+            //dd('pluto');
             session()->put('idCarrello', OrdineTestata::create(['tipo' => 0])->id);
         } 
-
-        $idCarrello = session('idCarrello');
 
 
         $ordine = new OrdineRiga;
@@ -66,3 +71,7 @@ class RigaOrdiniController extends Controller
         return redirect('/');
     }
 }
+
+// Spostare la getsione del carrelo da un altra parte
+
+// Se un Articolo e gia ce nel db viene riaggiunto sovrascriverlo aggiungendo la quantita totale 
